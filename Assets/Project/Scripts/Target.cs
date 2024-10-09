b84592d0a1dd2317300a62a39fec51c3440d6b2f
VinzR-2159109 <vinz.roosen@student.uhasselt.be>
2024-10-09 17:32:11 +0200

    Add movable Targets

--- Assets/Project/Scripts/Target.cs
+++ Assets/Project/Scripts/Target.cs
@@ -2,8 +2,80 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+    public enum MovementDirection
+    {
+        Left,
+        Right
+    }
+
+    [Header("Movement Settings")]
+    [SerializeField] MovementDirection movementDirection;
+    [SerializeField] bool isMovableTarget;
+    [SerializeField] float speed = 5.0f;
+    [SerializeField] float travelDistance = 5.0f;
+
+    [Header("Shooting Settings")]
     [SerializeField] int points = 10;
 
+    private Vector3 startPosition;
+    private Vector3 targetPosition;
+    private bool movingTowardsTarget = true;
+
+    void Start()
+    {
+        startPosition = transform.position;
+        SetInitialTargetPosition();
+    }
+
+    void Update()
+    {
+        if (isMovableTarget){
+            MoveTowardsTarget();
+
+            if (HasReachedTarget())
+            {
+                SwitchDirection();
+            }
+        }
+    }
+
+    private void SetInitialTargetPosition()
+    {
+        if (movementDirection == MovementDirection.Left)
+        {
+            targetPosition = startPosition - new Vector3(travelDistance, 0, 0);
+        }
+        else
+        {
+            targetPosition = startPosition + new Vector3(travelDistance, 0, 0);
+        }
+    }
+
+    private void MoveTowardsTarget()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+    }
+
+    private bool HasReachedTarget()
+    {
+        return Vector3.Distance(transform.position, targetPosition) < 0.01f;
+    }
+
+    private void SwitchDirection()
+    {
+        if (movingTowardsTarget)
+        {
+            targetPosition = startPosition - new Vector3(travelDistance, 0, 0);
+        }
+        else
+        {
+            targetPosition = startPosition + new Vector3(travelDistance, 0, 0);
+        }
+
+        movingTowardsTarget = !movingTowardsTarget;
+    }
+
+    
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Bullet"))
